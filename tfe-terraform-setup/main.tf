897052f06f4771dd22c1f0696f1d146fe9d6a4ef
petems <p.morsou+GIT@gmail.com>
2018-09-21 13:48:02 +0100

    Adding block for oauth

--- tfe-terraform-setup/main.tf
+++ tfe-terraform-setup/main.tf
@@ -1,10 +1,25 @@
 provider "tfe" {
+variable "oauth_token_id" {
+  type = "string"
 }
 
 resource "tfe_organization" "5mb_organisation" {
   name = "5mb-org-testing"
   email = "psouter@hashicorp.com"
 }
+
+# Use this to get the oauth-id, comment out afterwards
+# resource "tfe_workspace" "foo" {
+#   name          = "foo"
+#   organization  = "${tfe_organization.5mb_organisation.name}"
+
+#  # vcs_repo {
+#  #   identifier     = "petems/arbitary-terraform-code"
+#  #   oauth_token_id = "ot-dknb8WkWRsHg1q4p"
+#  #   branch         = "master"
+#  # }
+# }
+
 
 resource "tfe_workspace" "5mb_workspace" {
   name          = "5mb-workspace-${count.index}"
